1f772aec7fcd400b50bbb91549305a2014cc3886
Boris Katin <boris.katin@groupdocs.com>
2016-04-19 13:40:53 +0300

    Added Google documents sample

---
+++ GroupDocs.Conversion.Google/GoogleInputHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using GroupDocs.Conversion.Config;
+using GroupDocs.Conversion.Domain;
+using GroupDocs.Conversion.Handler.Input;
+using Newtonsoft.Json;
+
+namespace GroupDocs.Conversion.Google
+{
+    public class GoogleInputHandler : IInputDataHandler
+    {
+        private static string ClientId = ""; //TODO: Put you Google ClientId 
+        private static string ClientSecret = ""; //TODO: Put you Google ClientSecret 
+        private readonly DriveService _dataService;
+
+        private readonly ConversionConfig _conversionConfig;
+
+        public GoogleInputHandler(ConversionConfig conversionConfig)
+        {
+            _conversionConfig = conversionConfig;
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = ClientId,
+                    ClientSecret = ClientSecret
+                },
+                new[] { DriveService.Scope.DriveReadonly },
+                "user",
+                CancellationToken.None,
+                new FileDataStore("GoogleAuth", true));
+
+            _dataService = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential.Result
+            });
+        }
+        
+        public FileDescription GetFileDescription(string guid)
+        {
+            var googleDoc = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(Path.Combine(_conversionConfig.StoragePath, guid)));
+            string fileId = googleDoc.doc_id;
+
+            var file = _dataService.Files.Get(fileId).Execute();
+            
+            FileDescription result = new FileDescription();
+
+            result.Guid = file.Id;
+            result.Name = file.Name + ".pdf";
+            if (file.Size != null) result.Size = file.Size.Value;
+
+            return result;
+        }
+
+        public Stream GetFile(string guid)
+        {
+            var request = _dataService.Files.Export(guid, "application/pdf");
+            return request.ExecuteAsStream();
+        }
+    }
+}
---
+++ GroupDocs.Conversion.Google/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using GroupDocs.Conversion.Config;
+using GroupDocs.Conversion.Converter.Option;
+using GroupDocs.Conversion.Handler;
+
+namespace GroupDocs.Conversion.Google
+{
+    class Program
+    {
+        private static readonly string RootFolder = Path.GetFullPath("../../AppData");
+        private static readonly string ResultPath = Path.Combine(RootFolder, "ConvertedFiles");
+
+        static void Main(string[] args)
+        {
+            var storagePath = Path.Combine(RootFolder, "TestFiles");
+
+            // Set license
+            License license = new License();
+            license.SetLicense("");
+
+            // Setup Conversion configuration
+            var conversionConfig = new ConversionConfig
+            {
+                StoragePath = storagePath,
+                OutputPath = ResultPath,
+                UseCache = false
+            };
+
+            var inputDataHandler = new GoogleInputHandler(conversionConfig);
+            var conversionHandler = new ConversionHandler(conversionConfig, inputDataHandler);
+
+            var resultStream = conversionHandler.Convert<Stream>("document.gdoc", new WordsSaveOptions());
+            WriteStreamToFile(resultStream, "result.docx");
+            resultStream.Dispose();
+
+            Console.WriteLine("The conversion finished. Press <<ENTER>> to exit.");
+            Console.ReadLine();
+
+        }
+
+        private static void WriteStreamToFile(Stream stream, string fileName)
+        {
+            if (!Directory.Exists(ResultPath))
+            {
+                Directory.CreateDirectory(ResultPath);
+            }
+            stream.Position = 0;
+            using (
+                var fileStream = new FileInfo(Path.Combine(ResultPath, fileName)).Open(FileMode.Create,
+                    FileAccess.Write))
+            {
+                var buffer = new byte[16384];
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fileStream.Write(buffer, 0, read);
+                }
+            }
+        }
+    }
+}
